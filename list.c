b1bd7911a41bb7a0d8edf543b580425a6d3bb96d
Matangub <Matangub@gmail.com>
2016-09-20 00:13:57 +0300

    second commit

--- list.c
+++ list.c
@@ -6,19 +6,31 @@
 
 Node *newList(  ) {
 
-  Node *list = NULL;
-  // Node *list = malloc( sizeof(Node) );
+  // Node *list = NULL;
+  Node *list = malloc( sizeof(Node) );
   //
   // int *ap = malloc( sizeof(int) );
   // *ap = 5;
   // (*list).data = ap;
   // (*list).next = NULL;
+  (*list).data = NULL;
+  (*list).next = NULL;
 
   return list;
 }
 
 void destroyList(Node *list) {
-  list = NULL;
+  printf("Node destroyed!\n");
+  Node *nextNode = (Node*) (*list).next;
+
+  free(list);
+  (*list).data = NULL;
+  (*list).next = NULL;
+
+  if( nextNode ){
+    destroyList(nextNode);
+  }
+  // *list = NULL;
 }
 
 Node *getFinalNode(Node *list) {
@@ -38,66 +50,68 @@ Node *getFinalNode(Node *list) {
 
 void printListInt(Node *node) {
 
-  Node *myList = (Node*) node;
-  int *value = (int*) myList -> data;
+  // Node *myList = (Node*) node;
+  // int *value = (int*) myList -> data;
   // printf( "\nLast node value: %d\n", *value );
 
   if( node ) {
+    printf(" aaa ");
     // int *value = (int*) node -> data;
-    // printf("value: \n", *value);
+    // printf("value:%d \n", *value);
     // printListInt( node->next );
   }
 }
 
-void printLastNode(Node *list) {
-
-  Node *myStruct;
-  int *value;
-
-  if(1) {
-
-    if( getFinalNode(list) -> data ) {
-      myStruct = (Node*) getFinalNode(list);
-      value = (int*) myStruct -> data;
-      printf( "\nLast node value: %d\n", *value );
-    }
-    else {
-      printf("\nList is empty!");
-    }
-  }
-}
-
-void printFirstNode(Node *list) {
-
-  if(1) {
-
-    if( list -> data) {
-
-      int *value = (int*) list -> data;
-
-      printf( "\nfirst node value: %d\n", *value );
-    }
-    else {
-
-      printf( "\nfirst node is empty\n" );
-    }
-  }
-}
+// void printLastNode(Node *list) {
+//
+//   Node *myStruct;
+//   int *value;
+//
+//   if(1) {
+//
+//     if( getFinalNode(list) -> data ) {
+//       myStruct = (Node*) getFinalNode(list);
+//       value = (int*) myStruct -> data;
+//       printf( "\nLast node value: %d\n", *value );
+//     }
+//     else {
+//       printf("\nList is empty!");
+//     }
+//   }
+// }
+//
+// void printFirstNode(Node *list) {
+//
+//   if(1) {
+//
+//     if( list -> data) {
+//
+//       int *value = (int*) list -> data;
+//
+//       printf( "\nfirst node value: %d\n", *value );
+//     }
+//     else {
+//
+//       printf( "\nfirst node is empty\n" );
+//     }
+//   }
+// }
 
 void append( Node *list, void *value ) {
 
-  Node *newNode = (Node*) malloc( sizeof(Node) );
-  (*newNode).data = value;
-  (*newNode).next = NULL;
 
-  if(!list) {
+  if( !(*list).data && !(*list).next ) {
     printf("***no nodes...***\n" );
-    *list = *newNode;
+    (*list).data = value;
+    (*list).next = NULL;
   }
   else {
-    // printf("new node appended!\n");
-    //
-    // Node *finalNode = getFinalNode(list);
-    // (*finalNode).next = newNode;
+    Node *newNode = (Node*) malloc( sizeof(Node) );
+    (*newNode).data = value;
+    (*newNode).next = NULL;
+    printf("new node appended!\n");
+
+    Node *finalNode = getFinalNode(list);
+    (*finalNode).next = newNode;
   }
 }
--- main.c
+++ main.c
@@ -15,11 +15,25 @@ int main() {
   int a = 10;
   int *newData = &a;
   append(list, newData);
-  append(list, newData);
-  a = 20;
-  newData = &a;
+
+  int b = 20;
+  int *newData2 = &b;
+  append(list, newData2);
+
+  int c = 30;
+  int *newData3 = &c;
+  append(list, newData3);
+
+  int d = 40;
+  int *newData4 = &d;
+  append(list, newData4);
   // append(list, newData);
   // printNodes(list);
+  printListInt(list);
+
+  destroyList(list);
+  
+  printListInt(list);
   // printListInt(list);
   //
   // int b = 15;
